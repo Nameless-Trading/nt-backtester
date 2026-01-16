Migrated to bear-lake. Removed betas implementation. Fixed covariance matrix code.

nt_backtester/covariance_matrix.py
@@ -11,9 +11,12 @@ def load_data(name: str, date_: dt.date) -> pl.DataFrame:
     )
 
 
-def get_factor_loadings_matrix(factor_loadings: pl.DataFrame) -> np.ndarray:
+def get_factor_loadings_matrix(
+    tickers: list[str], factor_loadings: pl.DataFrame
+) -> np.ndarray:
     return (
-        factor_loadings.sort("ticker", "factor")
+        factor_loadings.filter(pl.col("ticker").is_in(tickers))
+        .sort("ticker", "factor")
         .pivot(index="ticker", on="factor", values="loading")
         .drop("ticker")
         .to_numpy()
@@ -29,8 +32,12 @@ def get_factor_covariance_matrix(factor_covariances: pl.DataFrame) -> np.ndarray
     )
 
 
-def get_idio_vol_matrix(idio_vol: pl.DataFrame) -> np.ndarray:
-    return np.diag(idio_vol.sort("ticker")["idio_vol"].to_numpy())
+def get_idio_vol_matrix(tickers: list[str], idio_vol: pl.DataFrame) -> np.ndarray:
+    return np.diag(
+        idio_vol.filter(pl.col("ticker").is_in(tickers))
+        .sort("ticker")["idio_vol"]
+        .to_numpy()
+    )
 
 
 def construct_covariance_matrix(
@@ -53,16 +60,16 @@ def construct_covariance_matrix(
     return covariance_matrix
 
 
-def get_covariance_matrix(date_: dt.date):
+def get_covariance_matrix(date_: dt.date, tickers: list[str]):
     factor_loadings = load_data("factor_loadings", date_)
     factor_covariances = load_data("factor_covariances", date_)
     idio_vol = load_data("idio_vol", date_)
 
     tickers = idio_vol["ticker"].to_list()
 
-    factor_loadings_matrix = get_factor_loadings_matrix(factor_loadings)
+    factor_loadings_matrix = get_factor_loadings_matrix(tickers, factor_loadings)
     factor_covariance_matrix = get_factor_covariance_matrix(factor_covariances)
-    idio_vol_matrix = get_idio_vol_matrix(idio_vol)
+    idio_vol_matrix = get_idio_vol_matrix(tickers, idio_vol)
 
     covariance_matrix = construct_covariance_matrix(
         factor_loadings_matrix, factor_covariance_matrix, idio_vol_matrix, tickers